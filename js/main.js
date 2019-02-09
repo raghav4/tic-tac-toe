f9c127ec1a018cd8ea5321239aa5f6f2bb7140f2
Raghav Sharma <raghavsyt@gmail.com>
2019-02-09 13:32:17 +0530

    updated JS

--- js/main.js
+++ js/main.js
@@ -1,21 +1,48 @@
-window.addEventListener("load",bindEvents);
-var isXorZero = false;
-function showTic(){
-    // this - Current Calling Button Reference
-    console.log("Show Tic Call");
-    if(this.innerText.trim().length==0)
-    {
-        this.innerText =  isXorZero?"⭕":"❌";
+let arr = [];
+let ctr = 0;
+let isXorZero = true;
+function reply_click(click_id){
+    let val = document.getElementById(click_id);
+    if(val.innerText.length==0){
+        ctr++;
+        val.innerText = isXorZero?"❌":"⭕";
+        arr[click_id] = val.innerText;
         isXorZero = !isXorZero;
+        arr[click_id] = val.innerText;
     }
-}
-
-function bindEvents(){
-var buttons = document.querySelectorAll("button");
-for(let button of buttons){
-    button.addEventListener("click",showTic);
-    //!button.showTic()
+    console.log(click_id);
+    if(checkWin(val.innerText)){
+        alert(val.innerText + " Wins! 😀");
+        location.reload();
+    }
+    if(ctr==9){
+        alert("It's a Draw!");
+        location.reload();
+    }
     
 }
+function checkWin(move){
+    var res1 = false;
+    if( checkRow(0,1,2,move) ||
+        checkRow(3,4,5,move) ||
+        checkRow(6,7,8,move) ||
+        checkRow(0,3,6,move) ||
+        checkRow(1,4,7,move) ||
+        checkRow(2,5,8,move) ||
+        checkRow(0,4,8,move) ||
+        checkRow(2,4,6,move)){
+            res1 = true;
+        }
+        return res1;
 }
-//bindEvents();
+function checkRow(a,b,c,move){
+    var res = false;
+    if(getbox(a)==move && getbox(b)==move && getbox(c)==move){
+        res = true;
+    }
+    return res;
+}
+function getbox(num){
+    console.log(document.getElementById(num).innerText);
+    return document.getElementById(num).innerText;
+}
